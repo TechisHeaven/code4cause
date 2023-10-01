62e8bcc6f37c3221cd296420bb05e1e8d5fa4d09
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2023-10-01 19:29:48 +0530

    Register bug

--- src/components/pages/Login/Register/Register.jsx
+++ src/components/pages/Login/Register/Register.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatchContext, useStateContext } from "../../../../store";
-import showToast from "../../../utils/Toast";
+import { showToast } from "../../../utils/Toast";
 
 const Register = () => {
   const { user } = useStateContext();
